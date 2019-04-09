f063ac698c7a8fac1b6f403852188d3d39bb29ef
im1sha <vkporsche@ya.ru>
2019-04-09 19:27:43 +0300

    tests added

--- Triangle/Triangle.Logic.Test/Negative.cs
+++ Triangle/Triangle.Logic.Test/Negative.cs
@@ -16,33 +16,22 @@ namespace Triangle.Logic.Test
         private MainPresenter presenter = new MainPresenter(null);
         private string[] stringArgs = new string[] { string.Empty, string.Empty, string.Empty };
 
-
         [TestCase("", "1", "1")]
-        public void IsInvalid(string a, string b, string c)
+        [TestCase("", "", "1")]
+        [TestCase("", "", "")]
+        [TestCase("0", "1", "1")]
+        [TestCase("0", "0", "1")]
+        [TestCase("0", "0", "0")]
+        [TestCase("9223372036854775808", "9223372036854775808", "9223372036854775808")]
+        [TestCase("9223372036854775808", "100", "9223372036854775808")]
+        [TestCase("9223372036854775808", "100", "100")]
+        public void CheckInput(string a, string b, string c)
         {
             Assert.Throws<ApplicationException>(() => presenter.GetTriangleType(a, b, c, stringArgs));
+            Assert.Throws<ApplicationException>(() => presenter.GetTriangleType(c, a, b, stringArgs));
+            Assert.Throws<ApplicationException>(() => presenter.GetTriangleType(b, c, a, stringArgs));
         }
     }
 }
 
     
-//1
-
-
-//1
-//1
-
-//0
-//1
-//1
-//0
-//0
-//1
-//0
-//9223372036854775808
-//9223372036854775808
-//100
-//9223372036854775808
-//100
-//9223372036854775808
-//100
--- Triangle/Triangle.Logic.Test/Type.cs
+++ Triangle/Triangle.Logic.Test/Type.cs
@@ -12,26 +12,9 @@ using Triangle.Core.Presenter;
 namespace Triangle.Logic.Test
 {
     [TestFixture]
-    class Positive
+    class Type
     {
         TypeDeterminer model = new TypeDeterminer();
-
-        [TestCase(SideHandler.MinValue, 2ul, 4ul, TriangleType.Invalid)]
-        [TestCase(4ul, 21ul, 42ul, TriangleType.Invalid)]
-        [TestCase(SideHandler.MaxValue, 10ul, 20ul, TriangleType.Invalid)]
-        [TestCase(SideHandler.MinValue, 2ul, 2ul + SideHandler.MinValue, TriangleType.Invalid)]
-        [TestCase(100ul, 200ul, 100ul + 200ul, TriangleType.Invalid)]
-        [TestCase(SideHandler.MaxValue, 100ul, SideHandler.MaxValue - 100, TriangleType.Invalid)]
-        public void IsInvalid(ulong a, ulong b, ulong c, TriangleType type)
-        {
-            Assert.AreEqual(type, model.GetTriangleType(a, b, c));
-            Assert.AreEqual(type, model.GetTriangleType(a, c, b));
-            Assert.AreEqual(type, model.GetTriangleType(b, a, c));
-            Assert.AreEqual(type, model.GetTriangleType(c, a, b));
-            Assert.AreEqual(type, model.GetTriangleType(b, c, a));
-            Assert.AreEqual(type, model.GetTriangleType(c, b, a));
-        }
-
 
         [TestCase(SideHandler.MinValue, SideHandler.MinValue, SideHandler.MinValue, TriangleType.Equilateral)]
         [TestCase(6ul, 6ul, 6ul, TriangleType.Equilateral)]
